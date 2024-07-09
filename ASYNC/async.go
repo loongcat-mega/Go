7f269da790dbeee359db15a46f8960c3e8f058b4
qingruixu <qingruixu@tencent.com>
2024-07-09 22:06:25 +0800

    常见的并发模式(未完待续)

--- ASYNC/async.go
+++ ASYNC/async.go
@@ -1,61 +1,60 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"runtime"
 	"sync"
-	"time"
 )
 
-type deletionApprovalItems struct {
-	approvalID int
-	articleID  int
-}
-
-var deletionPools = make(chan deletionApprovalItems, 10)
-var wg sync.WaitGroup
-
-func auditAsync(valuesRO <-chan int) {
-	defer wg.Done()
-	time.Sleep(3 * time.Second)
-	fmt.Println("start auditAsync")
-	value := <-valuesRO
-	deletionPools <- deletionApprovalItems{
-		approvalID: value,
-		articleID:  value,
-	}
-}
-func deleteAsync() {
-	defer wg.Done()
-	time.Sleep(500 * time.Millisecond)
-	v := <-deletionPools
-	fmt.Println(v)
-}
-func main() {
-
-	fmt.Println("start superDeletion")
-	values := make(chan int, 30)
-	wg.Add(1)
+// 返回生成自然数序列的管道: 2, 3, 4, ...
+func GenerateNatural(ctx context.Context, wg *sync.WaitGroup) chan int {
+	ch := make(chan int)
 	go func() {
 		defer wg.Done()
-		for {
-			var value int
-			fmt.Scan(&value)
-			values <- value
-			go auditAsync(values)
+		defer close(ch)
+		for i := 2; ; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- i:
+			}
 		}
 	}()
+	return ch
+}
 
-	fmt.Println("operate deletion pool articles")
-	wg.Add(1)
-	go deleteAsync()
-	wg.Add(1)
+// 管道过滤器: 删除能被素数整除的数
+func PrimeFilter(ctx context.Context, in <-chan int, prime int, wg *sync.WaitGroup) chan int {
+	out := make(chan int)
 	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-			fmt.Printf("当前的协程数量:%d\n", runtime.NumGoroutine())
+		defer wg.Done()
+		defer close(out)
+		for i := range in {
+			if i%prime != 0 {
+				select {
+				case <-ctx.Done():
+					return
+				case out <- i:
+				}
+			}
 		}
 	}()
-	wg.Wait()
+	return out
+}
 
+func main() {
+	wg := sync.WaitGroup{}
+	// 通过 Context 控制后台 Goroutine 状态
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	ch := GenerateNatural(ctx, &wg) // 自然数序列: 2, 3, 4, ...
+	for i := 0; i < 100; i++ {
+		prime := <-ch // 新出现的素数
+		fmt.Printf("%v: %v\n", i+1, prime)
+		wg.Add(1)
+		ch = PrimeFilter(ctx, ch, prime, &wg) // 基于新素数构造的过滤器
+	}
+
+	cancel()
+	wg.Wait()
 }
